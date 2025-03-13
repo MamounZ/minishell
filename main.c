cd22c5902807fb76f1cd50e07b62bcdf843a7099
Yaman Alrifai <yaman.alrfai@gmail.com>
2025-03-13 22:13:48 +0300

    fix main.c

--- main.c
+++ main.c
@@ -6,11 +6,7 @@
 /*   By: yaman-alrifai <yaman-alrifai@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 15:28:51 by mazaid            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/03/08 14:22:04 by mazaid           ###   ########.fr       */
-=======
-/*   Updated: 2025/03/07 11:43:55 by yaman-alrif      ###   ########.fr       */
->>>>>>> Yaman_work
+/*   Updated: 2025/03/13 22:13:32 by yaman-alrif      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,14 +36,6 @@ int main(int argc, char **argv, char **envp)
 		}
 		if (*input)
 			add_history(input);
-<<<<<<< HEAD
-		expanded_input = expand_variables(argv, input, ms, 1);
-		cmd_args = ft_split(expanded_input, ' '); // Here you should replaice this line with your Tokenizer so the builtin commands could be executede.
-
-		// Execute builtin command if it matches
-		if (cmd_args[0])
-			execute_builtin(cmd_args, ms);
-=======
 		copy_env(envp, ms);
 		expanded_input = expand_variables(argv, input, ms->envp_cpy, 1);
 		ms->tokens = tokenize(expanded_input); // Here you should replaice this line with your Tokenizer so the builtin commands could be executede.
@@ -57,7 +45,6 @@ int main(int argc, char **argv, char **envp)
 		// if (cmd_args[0])
 		// 	execute_builtin(cmd_args, ms);
 
->>>>>>> Yaman_work
 		free(input);
 		free(expanded_input);
 	}
